61fd2f3c2c791a392302c858786aa6193c33bea5
Nik Charlebois <nicharl@microsoft.com>
2021-05-20 17:26:21 -0400

    Update MGdcUtility.psm1

--- MGdcUtility.psm1
+++ MGdcUtility.psm1
@@ -43,143 +43,97 @@
     Write-Verbose -Message "Connecting to Microsoft Graph"
     Connect-MgGraph -AccessToken $AccessToken | Out-Null
 
-    Write-Verbose "Obtaining report file for {$Entity}"
-    $tempReportPath = Join-Path -Path $env:TEMP -ChildPath "graphreport.csv"
-    Remove-Item $tempReportPath -Force -ErrorAction SilentlyContinue
-    New-Item -Path $tempReportPath -Force | Out-Null
+    $allUsers = Get-MgUser -All:$true
 
-    #region Determine Period's Length
-    $period = "D180"
-    if ($NumberOfDays -le 7)
-    {
-        $period = "D7"
-    }
-    elseif ($NumberOfDays -gt 7 -and $NumberOfDays -le 30)
-    {
-        $period = "D30"
-    }
-    elseif ($NumberOfDays -gt 30 -and $NumberOfDays -le 90)
-    {
-        $period = "D90"
-    }
-    #endregion
-
-    #region Determine users in specified groups
-    [array]$users = @()
-    foreach ($groupID in $GroupsID)
-    {
-        $groupMembers = Get-MgGroupMember -GroupId $groupId
-
-        foreach ($member in $groupMembers)
-        {
-            $user = Get-MgUser -UserId $member.Id
-            $users += $user.UserPrincipalName
-        }
-    }
-    #endregion
-
-    switch($entity)
-    {
-        "Messages"
-        {
-            Write-Verbose -Message "Retrieving emails based on {$period}"
-            $url = "https://graph.microsoft.com/v1.0/reports/getMailboxUsageDetail(period='$period')"
-            Invoke-MgGraphRequest -Uri $url `
-                -OutputFilePath $tempReportPath | Out-Null
-            $parsedReport = Import-Csv $tempReportPath
-
-            $InboxEmailCount   = 0
-            $DeletedEmailCount = 0
-
-            # Count Emails for all users
-            foreach ($user in $parsedReport)
-            {
-                if (($users.Length -gt 0 -and $users.Contains($user.'User Principal Name')) -or $users.Length -eq 0)
-                {
-                    $InboxEmailCount += $user.'Item Count'
-                    $DeletedEmailCount += $user.'Deleted Item Count'
-                }
-            }
-
-            $results = @{
-                EmailsInInbox = $InboxEmailCount
-                DeletedEmails = $DeletedEmailCount
-                TotalItems = $InboxEmailCount + $DeletedEmailCount
-            }
-        }
-    }
-    return $results
-}
-
-function Get-MGdcEstimatedNumberOfItems2
-{
-    [OutputType([System.Collections.Hashtable])]
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory=$true)]
-        [System.String]
-        $AppId,
-        
-        [parameter(Mandatory=$true)]
-        [System.String]
-        $TenantId,
-        
-        [parameter(Mandatory=$true)]
-        [System.String]
-        $Secret,
-        
-        [parameter(Mandatory = $true)]
-        [System.String]
-        [ValidateSet('Messages')]
-        $Entity,
-
-        [parameter()]
-        [System.UInt32]
-        $NumberOfDays = 7,
-
-        [parameter()]
-        [System.String[]]
-        $GroupsID
-    )
-    $url = "https://login.microsoftonline.com/$TenantId/oauth2/v2.0/token"
-    $body = @{
-        scope = "https://graph.microsoft.com/.default"
-        grant_type = "client_credentials"
-        client_secret = $Secret
-        client_info = 1
-        client_id = $AppId
-    }
-    Write-Verbose -Message "Requesting Access Token for Microsoft Graph"
-    $OAuthReq = Invoke-RestMethod -Uri $url -Method Post -Body $body
-    $AccessToken = $OAuthReq.access_token
-
-    Write-Verbose -Message "Connecting to Microsoft Graph"
-    Connect-MgGraph -AccessToken $AccessToken | Out-Null
-
-    [array]$allUsers = Get-MgUser -All:$true
-
-    $foldersToFilterOut = @("Conversation History", "Sent Items", "Deleted Items")
+    $foldersToFilterOut = @()#@("Conversation History", "Sent Items", "Deleted Items")
 
     $total = 0
-    $index = 0
-    foreach ($user in $allUsers)
-    {
-        $mailFolders = Get-MgUserMailFolder -UserId $user.Id -ErrorAction SilentlyContinue
+    $jobName = "MGdcEstimateJob" + (New-Guid).ToString()
 
-        if ($null -ne $mailFolders)
-        {
-            foreach ($folder in $mailFolders)
+    $totalNumberOfUsers = $allUsers.Length
+    $numberOfParallelThreads = 4
+    $instances = Split-ArrayByParts -Array $allUsers `
+        -Parts $numberOfParallelThreads
+
+    foreach ($batch in $instances)
+    {
+        Start-Job -Name "$($jobName + $index)" -ScriptBlock {
+            param(
+                [System.Object[]]
+                $Batch,
+
+                [System.String]
+                $AccessToken
+            )            
+            Connect-MgGraph -AccessToken $AccessToken | Out-Null
+            $mailFolders = @()
+            foreach ($user in $Batch)
             {
-                if (-not $foldersToFilterOut.Contains($folder.DisplayName))
+                $mailFolders += Get-MgUserMailFolder -UserId $User.Id -ErrorAction SilentlyContinue
+            }
+            return $mailFolders
+        } -ArgumentList @($batch, $AccessToken) | Out-Null
+    }
+
+    do
+    {
+        [array]$pendingJobs = Get-Job | Where-Object -FilterScript { $_.Name -like 'MGdcEstimateJob*' -and $_.JobStateInfo.State -notin @('Complete', 'Blocked', 'Failed')}
+        [array]$CompletedJobs = Get-Job | Where-Object -FilterScript { $_.Name -like 'MGdcEstimateJob*' -and $_.JobStateInfo.State -eq 'Complete'}
+
+        foreach ($completedJob in $CompletedJobs)
+        {
+            $currentContent = Receive-Job -Name $completedJob.name
+            $mailFolders = $currentContent
+            Remove-Job -Name $completedJob.name -ErrorAction SilentlyContinue
+            if ($null -ne $mailFolders)
+            {
+                foreach ($folder in $mailFolders)
                 {
-                    Write-Progress -Activity "{$($user.UserPrincipalName)} -- {$($folder.DisplayName)}" -Status "Scanning" -PercentComplete ($index/$allUSers.Length*100)
-                    Write-Verbose -Message "{$($user.UserPrincipalName)}::{$($folder.DisplayName)}---> $($folder.TotalItemCount)"
-                    $total += $folder.TotalItemCount
+                    if (-not $foldersToFilterOut.Contains($folder.DisplayName))
+                    {
+                        $total += $folder.TotalItemCount
+                    }
                 }
             }
         }
-        $index++
-    }
 
+    } while ($pendingJobs.Length -gt 0)
+    
     return $total
+}
+
+function Split-ArrayByParts
+{
+    [OutputType([System.Object[]])]
+    param(
+        [Parameter(Mandatory = $true)]
+        [System.Object[]]
+        $Array,
+
+        [Parameter(Mandatory = $true)]
+        [System.Uint32]
+        $Parts
+    )
+
+    if ($Parts)
+    {
+        $PartSize = [Math]::Ceiling($Array.Count / $Parts)
+    }
+    $outArray = New-Object 'System.Collections.Generic.List[PSObject]'
+
+    for ($i = 1; $i -le $Parts; $i++)
+    {
+        $start = (($i - 1) * $PartSize)
+
+        if ($start -lt $Array.Count)
+        {
+            $end = (($i) * $PartSize) - 1
+            if ($end -ge $Array.count)
+            {
+                $end = $Array.count - 1
+            }
+            $outArray.Add(@($Array[$start..$end]))
+        }
+    }
+    return , $outArray
 }
